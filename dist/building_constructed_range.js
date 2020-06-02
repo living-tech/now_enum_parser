49667726fbe837a098a6e8ba82afb36970a44142
J.Itagaki <chaaabooo.firework.15@gmail.com>
2020-06-02 17:27:55 +0900

    add building_constructed_range function

---
+++ dist/building_constructed_range.js
@@ -0,0 +1,45 @@
+"use strict";
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+var generators_1 = require("./lib/generators");
+var BuildingConstructedRange;
+(function (BuildingConstructedRange) {
+    BuildingConstructedRange["OneYear"] = "OneYear";
+    BuildingConstructedRange["ThreeYear"] = "ThreeYear";
+    BuildingConstructedRange["FiveYear"] = "FiveYear";
+    BuildingConstructedRange["SevenYear"] = "SevenYear";
+    BuildingConstructedRange["TenYear"] = "TenYear";
+    BuildingConstructedRange["FifteenYear"] = "FifteenYear";
+    BuildingConstructedRange["TwentyYear"] = "TwentyYear";
+    BuildingConstructedRange["TwentyFiveYear"] = "TwentyFiveYear";
+    BuildingConstructedRange["ThirtyYear"] = "ThirtyYear";
+})(BuildingConstructedRange = exports.BuildingConstructedRange || (exports.BuildingConstructedRange = {}));
+var translations = {
+    ja: (_a = {},
+        _a[BuildingConstructedRange.OneYear] = '1年以内',
+        _a[BuildingConstructedRange.ThreeYear] = '3年以内',
+        _a[BuildingConstructedRange.FiveYear] = '5年以内',
+        _a[BuildingConstructedRange.SevenYear] = '7年以内',
+        _a[BuildingConstructedRange.TenYear] = '10年以内',
+        _a[BuildingConstructedRange.FifteenYear] = '15年以内',
+        _a[BuildingConstructedRange.TwentyYear] = '20年以内',
+        _a[BuildingConstructedRange.TwentyFiveYear] = '25年以内',
+        _a[BuildingConstructedRange.ThirtyYear] = '30年以内',
+        _a),
+    en: (_b = {},
+        _b[BuildingConstructedRange.OneYear] = 'Within a year',
+        _b[BuildingConstructedRange.ThreeYear] = 'Within three years',
+        _b[BuildingConstructedRange.FiveYear] = 'Within five years',
+        _b[BuildingConstructedRange.SevenYear] = 'Within seven years',
+        _b[BuildingConstructedRange.TenYear] = 'Within ten years',
+        _b[BuildingConstructedRange.FifteenYear] = 'Within fifteen years',
+        _b[BuildingConstructedRange.TwentyYear] = 'Within twenty years',
+        _b[BuildingConstructedRange.TwentyFiveYear] = 'Within twentyfive years',
+        _b[BuildingConstructedRange.ThirtyYear] = 'Within Thirty years',
+        _b),
+};
+var reverseTranslations = generators_1.generateReverseTranslations(translations);
+// Edit constants name
+exports.BuildingConstructedRangeEncoder = generators_1.generateEncoder(translations);
+exports.BuildingConstructedRangeDecoder = generators_1.generateDecoder(reverseTranslations);
+//# sourceMappingURL=building_constructed_range.js.map
---
+++ src/building_constructed_range.ts
@@ -0,0 +1,53 @@
+import { Translations } from './lib/interfaces';
+import {
+  generateReverseTranslations,
+  generateEncoder,
+  generateDecoder,
+} from './lib/generators';
+
+export enum BuildingConstructedRange {
+  OneYear = 'OneYear',                 // 1年以内
+  ThreeYear = 'ThreeYear',             // 3年以内
+  FiveYear = 'FiveYear',               // 5年以内
+  SevenYear = 'SevenYear',             // 7年以内
+  TenYear = 'TenYear',                 // 10年以内
+  FifteenYear = 'FifteenYear',         // 15年以内
+  TwentyYear = 'TwentyYear',           // 20年以内
+  TwentyFiveYear = 'TwentyFiveYear',   // 25年以内
+  ThirtyYear = 'ThirtyYear',           // 30年以内
+}
+
+const translations: Translations = {
+  ja: {
+    [BuildingConstructedRange.OneYear]: '1年以内',
+    [BuildingConstructedRange.ThreeYear]: '3年以内',
+    [BuildingConstructedRange.FiveYear]: '5年以内',
+    [BuildingConstructedRange.SevenYear]: '7年以内',
+    [BuildingConstructedRange.TenYear]: '10年以内',
+    [BuildingConstructedRange.FifteenYear]: '15年以内',
+    [BuildingConstructedRange.TwentyYear]: '20年以内',
+    [BuildingConstructedRange.TwentyFiveYear]: '25年以内',
+    [BuildingConstructedRange.ThirtyYear]: '30年以内',
+  },
+  en: {
+    [BuildingConstructedRange.OneYear]: 'Within a year',
+    [BuildingConstructedRange.ThreeYear]: 'Within three years',
+    [BuildingConstructedRange.FiveYear]: 'Within five years',
+    [BuildingConstructedRange.SevenYear]: 'Within seven years',
+    [BuildingConstructedRange.TenYear]: 'Within ten years',
+    [BuildingConstructedRange.FifteenYear]: 'Within fifteen years',
+    [BuildingConstructedRange.TwentyYear]: 'Within twenty years',
+    [BuildingConstructedRange.TwentyFiveYear]: 'Within twentyfive years',
+    [BuildingConstructedRange.ThirtyYear]: 'Within Thirty years',
+  },
+};
+
+const reverseTranslations = generateReverseTranslations(translations);
+
+// Edit constants name
+export const BuildingConstructedRangeEncoder = generateEncoder<BuildingConstructedRange>(
+  translations,
+);
+export const BuildingConstructedRangeDecoder = generateDecoder<BuildingConstructedRange>(
+  reverseTranslations,
+);
