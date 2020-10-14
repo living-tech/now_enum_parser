196ddb7194a881b3a4108be4ca34917e8064a59b
uekishun <ueki.shun.123@gmail.com>
2020-10-14 10:33:01 +0900

    fix: TenancyTerm (#50)

--- src/tenancy_term.ts
+++ src/tenancy_term.ts
@@ -17,14 +17,14 @@ export enum TenancyTerm {
 // Edit contexts
 const translations: Translations = {
   ja: {
-    [TenancyTerm.LessThanOneMonth]: '1ヶ月未満',
+    [TenancyTerm.LessThanOneMonth]: '1週間以上～1ヶ月未満',
     [TenancyTerm.OneToThreeMonths]: '1ヶ月以上～3ヶ月未満',
     [TenancyTerm.ThreeToSevenMonths]: '3ヶ月以上～7ヶ月未満',
     [TenancyTerm.SevenMonthsToOneYear]: '7ヶ月以上～1年未満',
     [TenancyTerm.MoreThanOneYear]: '1年以上',
   },
   en: {
-    [TenancyTerm.LessThanOneMonth]: 'less than one month',
+    [TenancyTerm.LessThanOneMonth]: 'one week to one month',
     [TenancyTerm.OneToThreeMonths]: 'one to three months',
     [TenancyTerm.ThreeToSevenMonths]: 'three to seven months',
     [TenancyTerm.SevenMonthsToOneYear]: 'seven months to one year',
