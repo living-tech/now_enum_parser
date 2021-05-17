7deec9db75f30d2dd9d8816fb6d9c45184eb6624
Youbing Ding <youbing.ding@nowroom.jp>
2021-05-17 17:31:15 +0900

    add HomesType

---
+++ dist/homes_type.js
@@ -0,0 +1,42 @@
+"use strict";
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.homesTypeDecoder = exports.homesTypeEncoder = exports.HomesType = void 0;
+var generators_1 = require("./lib/generators");
+var HomesType;
+(function (HomesType) {
+    HomesType["Mansion"] = "Mansion";
+    HomesType["Apartment"] = "Apartment";
+    HomesType["House"] = "House";
+    HomesType["TerracedHouse"] = "TerracedHouse";
+    HomesType["Townhouse"] = "TownHouse";
+    HomesType["ShareHouse"] = "ShareHouse";
+    HomesType["Dormitory"] = "Dormitory";
+})(HomesType = exports.HomesType || (exports.HomesType = {}));
+// Edit contexts
+var translations = {
+    ja: (_a = {},
+        _a[HomesType.Mansion] = 'マンション',
+        _a[HomesType.Apartment] = 'アパート',
+        _a[HomesType.House] = '一戸建',
+        _a[HomesType.TerracedHouse] = 'テラスハウス',
+        _a[HomesType.Townhouse] = 'タウンハウス',
+        _a[HomesType.ShareHouse] = 'シェアハウス',
+        _a[HomesType.Dormitory] = '寮・下宿',
+        _a),
+    en: (_b = {},
+        _b[HomesType.Mansion] = 'Mansion',
+        _b[HomesType.Apartment] = 'Apartment',
+        _b[HomesType.House] = 'House',
+        _b[HomesType.TerracedHouse] = 'Terraced House',
+        _b[HomesType.Townhouse] = 'Town House',
+        _b[HomesType.ShareHouse] = 'Share House',
+        _b[HomesType.Dormitory] = 'Dormitory',
+        _b),
+};
+// DO NOT EDIT
+var reverseTranslations = generators_1.generateReverseTranslations(translations);
+// Edit constants name
+exports.homesTypeEncoder = generators_1.generateEncoder(translations);
+exports.homesTypeDecoder = generators_1.generateDecoder(reverseTranslations);
+//# sourceMappingURL=homes_type.js.map
---
+++ src/homes_type.ts
@@ -0,0 +1,49 @@
+import { Translations } from './lib/interfaces';
+import {
+  generateReverseTranslations,
+  generateEncoder,
+  generateDecoder,
+} from './lib/generators';
+
+export enum HomesType {
+  Mansion = 'Mansion', // 【居住用賃貸】3101:マンション 3102:アパート 3103:一戸建 3104:テラスハウス 3105:タウンハウス 3106:シェアハウス 3110:寮・下宿
+  Apartment = 'Apartment', //
+  House = 'House', //
+  TerracedHouse = 'TerracedHouse', //
+  Townhouse = 'TownHouse', //
+  ShareHouse = 'ShareHouse', //
+  Dormitory = 'Dormitory', //
+}
+
+// Edit contexts
+const translations: Translations = {
+  ja: {
+    [HomesType.Mansion]: 'マンション', // 【居住用賃貸】3101:マンション 3102:アパート 3103:一戸建 3104:テラスハウス 3105:タウンハウス 3106:シェアハウス 3110:寮・下宿
+    [HomesType.Apartment]: 'アパート',
+    [HomesType.House]: '一戸建',
+    [HomesType.TerracedHouse]: 'テラスハウス',
+    [HomesType.Townhouse]: 'タウンハウス',
+    [HomesType.ShareHouse]: 'シェアハウス',
+    [HomesType.Dormitory]: '寮・下宿',
+  },
+  en: {
+    [HomesType.Mansion]: 'Mansion',
+    [HomesType.Apartment]: 'Apartment',
+    [HomesType.House]: 'House',
+    [HomesType.TerracedHouse]: 'Terraced House',
+    [HomesType.Townhouse]: 'Town House',
+    [HomesType.ShareHouse]: 'Share House',
+    [HomesType.Dormitory]: 'Dormitory',
+  },
+};
+
+// DO NOT EDIT
+const reverseTranslations = generateReverseTranslations(translations);
+
+// Edit constants name
+export const homesTypeEncoder = generateEncoder<HomesType>(
+  translations,
+);
+export const homesTypeDecoder = generateDecoder<HomesType>(
+  reverseTranslations,
+);
