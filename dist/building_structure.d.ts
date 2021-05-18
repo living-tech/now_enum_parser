386a382dc2357c3ed20c1ad8b00fe77f2ebc0061
Youbing Ding <youbing.ding@nowroom.jp>
2021-05-18 14:55:24 +0900

    yarn build

--- dist/building_structure.d.ts
+++ dist/building_structure.d.ts
@@ -2,7 +2,15 @@ export declare enum BuildingStructure {
     Wood = "Wood",
     Stell = "Stell",
     Rc = "Rc",
-    Src = "Src"
+    Src = "Src",
+    Block = "Block",
+    PC = "PC",
+    HPC = "HPC",
+    Other = "Other",
+    LightStell = "LightStell",
+    Alc = "Alc",
+    RBlock = "RBlock",
+    CFT = "CFT"
 }
 export declare const buildingStructureEncoder: (raw: BuildingStructure, lang: import(".").Language) => string;
 export declare const buildingStructureDecoder: (text: string, lang: import(".").Language) => BuildingStructure;
