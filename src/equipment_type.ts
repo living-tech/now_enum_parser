bf73e85beeeff128c1b57cd7a20e2e3eab74bf5a
sj-park <sejong.park@nowroom.jp>
2022-01-05 20:40:42 +0900

    yarn format and build

--- src/equipment_type.ts
+++ src/equipment_type.ts
@@ -7,15 +7,15 @@ import {
 
 // Edit enum name and contents
 export enum EquipmentType {
-  Internet = "Internet",
-  AutoLock = "AutoLock",
-  AirConditioner = "AirConditioner",
-  Closet = "Closet",
-  SeparateBathroomAndToilet = "SeparateBathroomAndToilet",
-  IndependentWashBasin ="IndependentWashBasin",
-  ParkingLotIncluded = "ParkingLotIncluded",
-  DeliveryBox = "DeliveryBox",
-  Pet = "Pet"
+  Internet = 'Internet',
+  AutoLock = 'AutoLock',
+  AirConditioner = 'AirConditioner',
+  Closet = 'Closet',
+  SeparateBathroomAndToilet = 'SeparateBathroomAndToilet',
+  IndependentWashBasin = 'IndependentWashBasin',
+  ParkingLotIncluded = 'ParkingLotIncluded',
+  DeliveryBox = 'DeliveryBox',
+  Pet = 'Pet',
 }
 
 // Edit contexts
